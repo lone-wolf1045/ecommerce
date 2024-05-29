944fee0abe7261e79e1938956584e860bf0a3fe4
Hasanul Banna <hasanul@gmail.com>
2024-05-29 23:26:20 +0600

    change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -58,6 +58,54 @@ return redirect()->back()->with('message','Product Added Successfully');
 
 }
 
+public function show_product(){
+   $product=product::all();
+   return view('admin.show_product',compact('product'));
+
+
+}
+ public function delete_product($id){
+$product=product::find($id);
+$product->delete();
+return redirect()->back()->with('message','Product Deleted Successfully');
+
+ }
+ public function update_product($id){
+   $product=product::find($id);
+$catagory=catagory::all();
+
+return view('admin.update_product',compact('product','catagory'));
+
+
+ }
+
+ public function update_product_confirm(Request $request,$id){
+$product=product::find($id);
+$product->title=$request->title;
+$product->description=$request->description;
+$product->price=$request->price;
+$product->discount_price=$request->dis_price;
+$product->catagory=$request->catagory;
+$product->quantity=$request->quantity;
+$image=$request->image;
+
+if($image){
+   $imagename=time().'.'.$image->getClientOriginalExtension();
+   $request->image->move('product',$imagename);
+   
+   $product->image=$imagename;
+
+
+}
+
+$product->save();
+return redirect()->back()->with('message','Product Updated Successfully');
+
+
+
+ }
+
+
 
 
 }
